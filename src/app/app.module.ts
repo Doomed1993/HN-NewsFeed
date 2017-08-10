5a0c6dcb92c25705725da63a089e751d93c0c564
Doomed93 <priyadsg@gmail.com>
2017-08-10 18:17:34 +0530

    Final Version of HN NewsFeed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+//import { StoriesComponent } from './stories/stories.component';
+import { FooterComponent } from './footer/footer.component';
+import {HackerNewsAPIService} from "./hackernews-api.service";
+import { StoriesComponent } from './stories/stories.component';
+import {ItemComponent} from "./item/item.component";
+import { ItemCommentsComponent } from './item-comments/item-comments.component';
+import {routing} from "./app.routes";
+import { CommentComponent } from './comment/comment.component';
+import { CommentTreeComponent } from './comment-tree/comment-tree.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ItemComponent,
+    StoriesComponent,
+    ItemCommentsComponent,
+    CommentComponent,
+    CommentTreeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    routing
   ],
-  providers: [],
+  providers: [HackerNewsAPIService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
